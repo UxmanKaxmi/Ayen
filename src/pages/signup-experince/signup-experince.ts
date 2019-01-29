7d0de9e684848d161e49e6e99d57fa299a22f637
Uxman <ZAKI_US94@HOTMAIL.COM>
2019-01-29 18:04:36 +0500

    Latest

--- src/pages/signup-experince/signup-experince.ts
+++ src/pages/signup-experince/signup-experince.ts
@@ -6,7 +6,7 @@ import { AbstractControl, FormBuilder, Validators, FormGroup } from '@angular/fo
 import { HelpersProvider } from './../../providers/helpers/helpers';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { File } from '@ionic-native/file';
+import { File, FileEntry } from '@ionic-native/file';
 import { FileChooser } from '@ionic-native/file-chooser';
 import { Platform } from 'ionic-angular';
 import { IOSFilePicker } from '@ionic-native/file-picker/ngx';
@@ -33,23 +33,28 @@ export class SignupExperincePage {
   degreePrefession: string;
   focusExperience: string;
 
-  licenseInButtonText :string = "Upload your License..."
-  CVInButtonText:string= "Upload your CV..."
+  licenseInButtonText: string = "Upload your License..."
+  CVInButtonText: string = "Upload your CV..."
 
   sceNumber: AbstractControl;
   formgroup: FormGroup;
   briefDescription: AbstractControl;
   expiryDateValue: string;
   defaultDate: any;
-  filePath:any;
-  signUpDataPersonal:any;
+  filePath_forButtonText: any;
+  signUpDataPersonal: any;
   dataList: any = "";
   willshowOtherTextField: boolean = false;
   membership: string;
   value: number;
+  cvUri: any;
+  licenseUri: any;
+  filesPath: any;
+  fileType: any;
+  filesName: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private helpers: HelpersProvider,
-    public formbuilder: FormBuilder, private datePicker: DatePicker,private transfer: FileTransfer, private file: File,public fileChooser: FileChooser, public platform:Platform,public filePicker: IOSFilePicker,public api:ApiProvider
+    public formbuilder: FormBuilder, private datePicker: DatePicker, private transfer: FileTransfer, private file: File, public fileChooser: FileChooser, public platform: Platform, public filePicker: IOSFilePicker, public api: ApiProvider
   ) {
     this.setDefaultDate();
 
@@ -85,7 +90,7 @@ export class SignupExperincePage {
       console.log('DISABLED:SHOW OTHER BOX')
 
     }
-    this.signUpDataPersonal =this.navParams.get('signUpDataPersonal')
+    this.signUpDataPersonal = this.navParams.get('signUpDataPersonal')
     console.log('data from signUpDataPersonal', this.signUpDataPersonal)
 
   }
@@ -114,32 +119,34 @@ export class SignupExperincePage {
       let dataFromSignupExperience = {
         AcademicMajor: this.academicMajor,
         YearsOfExperience: this.yearsOfExperience,
-        SceMemberShipNumber:this.sceNumber.value,
-        SceExpiryDate:this.expiryDateValue,
-        ProfessionalDegree:this.degreePrefession,
-        FocusExperience:this.focusExperience,
-        Description:this.briefDescription.value
-
+        SceMemberShipNumber: this.sceNumber.value,
+        SceExpiryDate: this.expiryDateValue,
+        ProfessionalDegree: this.degreePrefession,
+        FocusExperience: this.focusExperience,
+        Description: this.briefDescription.value,
+        cvUri: this.cvUri,
+        licenseUri: this.licenseUri
 
 
       }
-      Object.assign(this.signUpDataPersonal,dataFromSignupExperience);
+      Object.assign(this.signUpDataPersonal, dataFromSignupExperience);
       console.log(this.signUpDataPersonal)
 
-      this.api.signUpService("signup",this.signUpDataPersonal).then(response => {
-        this.dataList = response;
+      // this.api.signUpService("signup",this.signUpDataPersonal).then(response => {
+      //   this.dataList = response;
 
-        if(this.dataList.message=="User Verified"){
-          alert("LOGIN SUCCESS")
-        }
-          else {
-            alert(this.dataList.message)
+      //   if(this.dataList.message=="User Verified"){
+      //     alert("LOGIN SUCCESS")
+      //   }
+      //     else {
+      //       alert(this.dataList.message)
 
-          }
+      //     }
 
 
 
-      });
+      // });
+      this.api.uploadFile(this.signUpDataPersonal)
 
 
       // this.navCtrl.push(ConfirmationCodeSuccessPage,{},{animate:false})
@@ -198,12 +205,10 @@ export class SignupExperincePage {
   }
 
   onChange(membership) {
-    if (membership == 'other')
-    {
+    if (membership == 'other') {
       this.value = 1;
     }
-    else
-    {
+    else {
       this.value = undefined;
     }
   }
@@ -217,44 +222,62 @@ export class SignupExperincePage {
 
 
   upload_cv() {
+    // this.platform.ready().then(() => {
+
+
+    //   if (this.platform.is('ios')) {
+    //     console.log("running on iOS device!");
+
+    //     this.filePicker.pickFile()
+    //       .then(uri => console.log(uri))
+    //       .catch(err => console.log('Error', err));
+    //   }
+
+
+    // });
+
+
+    // this.fileChooser.open()
+    //   .then(url => this.setPath_cv(url))
+    //   .catch(e => alert(e));
 
 
 
-    this.platform.ready().then(() => {
+    this.fileChooser.open().then(uri =>
+      {
+        console.log(uri);
+        // debugger;
+        // this.filePath.resolveNativePath(uri).then(filePath =>
+        //   {
+        //     this.filesPath = filePath;
+        //     this.file.resolveLocalFilesystemUrl(filePath).then(fileInfo =>
+        //       {
+        //         let files = fileInfo as FileEntry;
+        //         files.file(success =>
+        //           {
+        //             this.fileType   = success.type;
+        //             this.filesName  = success.name;
+        //           });
+        //       },err =>
+        //       {
+        //         console.log(err);
+        //         throw err;
+        //       });
+        //   },err =>
+        //   {
+        //     console.log(err);
+        //     throw err;
+        //   });
+      },err =>
+      {
+        console.log(err);
+        throw err;
+      });
 
 
-      if (this.platform.is('ios')) {
-          console.log("running on iOS device!");
-
-          this.filePicker.pickFile()
-          .then(uri => console.log(uri))
-          .catch(err => console.log('Error', err));
-        }
 
 
-  });
 
-
-  //   debugger;
-  //   const fileTransfer: FileTransferObject = this.transfer.create();
-
-  //   let options: FileUploadOptions = {
-  //      fileKey: 'file',
-  //      fileName: 'name.jpg',
-  //      headers: {}
-
-  //     }
-
-  //   fileTransfer.upload('asdasd', 'http://192.168.0.7:8080/api/uploadImage', options)
-  //    .then((data) => {
-  //      // success
-  //    }, (err) => {
-  //      // error
-  //    })
-
-  this.fileChooser.open()
-  .then(url => this.setPath_cv(url))
-  .catch(e => alert(e));
   }
 
   upload_lisence() {
@@ -262,64 +285,68 @@ export class SignupExperincePage {
 
 
       if (this.platform.is('ios')) {
-          console.log("running on iOS device!");
-          this.filePicker.pickFile()
+        console.log("running on iOS device!");
+        this.filePicker.pickFile()
           .then(uri => console.log(uri))
           .catch(err => console.log('Error', err));
-        }
+      }
 
 
-  });
+    });
 
-    //   console.log('clickrd')
-    //   debugger;
-    //   const fileTransfer: FileTransferObject = this.transfer.create();
+    console.log('clickrd')
+    debugger;
+    const fileTransfer: FileTransferObject = this.transfer.create();
 
-    //   let options: FileUploadOptions = {
-    //      fileKey: 'file',
-    //      fileName: 'name.jpg',
-    //      headers: {}
+    let options: FileUploadOptions = {
+      fileKey: 'file',
+      fileName: 'name.jpg',
+      headers: {}
 
-    //     }
+    }
 
-    //   fileTransfer.upload('asdasd', 'http://192.168.0.7:8080/api/uploadImage', options)
-    //    .then((data) => {
-    //      // success
-    //    }, (err) => {
-    //      // error
-    //    })
+    fileTransfer.upload('asdasd', 'http://192.168.0.7:8080/api/uploadImage', options)
+      .then((data) => {
+        // success
+      }, (err) => {
+        // error
+      })
 
     this.fileChooser.open()
-    .then(url => this.setPath_license(url))
-    .catch(e => alert(e));
-    }
+      .then(url => this.setPath_license(url))
+      .catch(e => alert(e));
+  }
 
-  checkLength_sceNumber(e,length){
+  checkLength_sceNumber(e, length) {
     console.log(e)
-    if(this.sceNumber.value.length>length){
+    if (this.sceNumber.value.length > length) {
 
       //to remove all the chracter after 10 digits
-    let str = this.sceNumber.value.substring(0, length);
-    this.formgroup.controls['sceNumber'].setValue(str);
+      let str = this.sceNumber.value.substring(0, length);
+      this.formgroup.controls['sceNumber'].setValue(str);
     }
   }
-  setPath_license(url){
-
+  setPath_license(url) {
+    this.licenseUri = url;
+    console.log('licenseUri from setPAth function', this.licenseUri);
     console.log(url);
-    let var1= url.lastIndexOf("/") +1;
+    let var1 = url.lastIndexOf("/") + 1;
     let var2 = url.substring(var1);
-     this.licenseInButtonText= var2;
+    this.licenseInButtonText = var2;
 
 
 
 
   }
-  setPath_cv(url){
+  setPath_cv(url) {
+    console.log('the Path is', url)
 
-    console.log(url);
-    let var1= url.lastIndexOf("/") +1;
+
+    this.cvUri = url;
+    console.log('cvURI from setPAth function', this.cvUri);
+    let var1 = url.lastIndexOf("/") + 1;
     let var2 = url.substring(var1);
-     this.CVInButtonText= var2;
+    this.CVInButtonText = var2;
 
 
 
--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,3 +1,5 @@
+import { LoadingController } from 'ionic-angular';
+import { FileTransfer, FileTransferObject, FileUploadOptions } from '@ionic-native/file-transfer';
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 
@@ -5,13 +7,15 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 @Injectable()
 export class ApiProvider {
 
-  APILink: string = 'http://ayen.apptech.com.tr/ayen/Api/';
+  // APILink: string = 'http://ayen.apptech.com.tr/ayen/Api/';
+  APILink: string = 'http://192.168.0.100/ayen/Api/';
   body: any;
+  bodyExample: any;
+  body2: any;
 
   // bodyJSON:JSON
 
-  constructor(public http: Http) {
-    console.log('Hello ApiProvider Provider');
+  constructor(public http: Http,private transfer: FileTransfer,public loadingctrl:LoadingController) {
   }
   //LOGIN SERVICE
   public loginService(afterLink,body) {
@@ -35,25 +39,124 @@ export class ApiProvider {
 
 //SIGNUP SERVICE
   public signUpService(afterLink,body) {
-
-
     console.log("The body being send in signup",body)
-    debugger;
-    return new Promise((resolve, reject) => {
-      let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
-      let options = new RequestOptions({ headers: headers });
-      this.http.post(this.APILink + afterLink , body, options)
-        .toPromise()
-        .then((response) => {
-          console.log('API Response from '+afterLink+ ': ', response.json());
-          resolve(response.json());
-        })
-        .catch((error) => {
-          console.error('API Error from '+afterLink+ ': ', error.status);
-          console.error('API Error from '+afterLink+ ', ', JSON.stringify(error));
-          reject(error);
-        });
+    this.uploadFile(body)
+    // return new Promise((resolve, reject) => {
+    //   let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+    //   let options = new RequestOptions({ headers: headers });
+    //   this.http.post(this.APILink + afterLink , body, options)
+    //     .toPromise()
+    //     .then((response) => {
+    //       console.log('API Response from '+afterLink+ ': ', response.json());
+    //       resolve(response.json());
+    //     })
+    //     .catch((error) => {
+    //       console.error('API Error from '+afterLink+ ': ', error.status);
+    //       console.error('API Error from '+afterLink+ ', ', JSON.stringify(error));
+    //       reject(error);
+    //     });
+    // });
+  }
+
+
+
+  uploadFile(body) {
+
+debugger;
+      this.body2 =  "Email=" +body.Email +
+                  "&EmailConfirmed=" +'true'+
+                  "&Password=" +body.Password+
+                  "&Prefix=" + body.Prefix +
+                  "&FirstName=" + body.FirstName +
+                  "&LastName=" + body.LastName +
+                  "&PhoneNumber=" + body.PhoneNumber +
+                  "&PhoneNumberConfirmed=" + 'true' +
+                  "&Gender=" + body.Gender +
+                  "&AcademicMajor=" + body.AcademicMajor +
+                  "&YearsOfExperience=" + body.YearsOfExperience +
+                  "&ProfessionalDegree=" + body.ProfessionalDegree +
+                  "&FocusExperience=" + body.FocusExperience +
+                  "&Description=" + body.Description +
+                  "&Region=" + body.Region +
+                  "&City=" + body.City +
+                  "&OrganizationName=" + body.OrganizationName +
+                  "&CRNumber=" + body.CRNumber +
+                  "&OrgLicenceNumber=" + body.OrgLicenceNumber +
+                  "&OrgExpiryDate=" + body.OrgExpiryDate +
+                  "&RoleType=" + body.RoleType +
+                  "&SceMemberShipNumber=" + body.SceMemberShipNumber +
+                  "&SceExpiryDate=" + body.SceExpiryDate +
+                  "&NationalityId=" + body.NationalityId ;
+                  // "&OrgLicence=" + body.licenseUri +
+                  // "&Cv=" + body.cvUri ;
+
+
+
+    console.log(body)
+    let loader = this.loadingctrl.create({
+      content: "Uploading..."
+    });
+    loader.present();
+    const fileTransfer: FileTransferObject = this.transfer.create();
+
+    let options: FileUploadOptions = {
+      fileKey: 'Cv',
+      fileName: 'Cv',
+
+      chunkedMode: false,
+      mimeType: "multipart/form-data",
+      params : {
+        "Email": body.Email,
+        "EmailConfirmed": 'true',
+        "Password": body.Password,
+        "Prefix": body.Prefix,
+        "FirstName": body.FirstName,
+        "LastName": body.LastName,
+        "PhoneNumber" : body.PhoneNumber ,
+        "PhoneNumberConfirmed" : body.PhoneNumberConfirmed ,
+        "Gender":body.Gender ,
+        "AcademicMajor":body.AcademicMajor ,
+        "YearsOfExperience":body.YearsOfExperience ,
+        "ProfessionalDegree":body.ProfessionalDegree ,
+        "FocusExperience":body.FocusExperience ,
+        "Description":body.Description ,
+        "Nationality":body.Nationality ,
+        "Region":body.Region ,
+        "City":body.City ,
+        "OrganizationName":body.OrganizationName ,
+        "CRNumber":body.CRNumber ,
+        "OrgLicenceNumber":body.OrgLicenceNumber ,
+        "OrgExpiryDate":body.OrgExpiryDate ,
+        "RoleType":body.RoleType ,
+        "SceMemberShipNumber":body.SceMemberShipNumber ,
+        "SceExpiryDate":body.SceExpiryDate ,
+        "NationalityId":body.NationalityId ,
+
+
+
+
+      }
+
+
+
+    }
+    // fileTransfer.onProgress((e)=>
+    // {
+    //     this.prg=(e.lengthComputable) ?  Math.round((e.loaded * 100) / e.total) : -1;
+    //     this.changeDetectorRef.detectChanges();
+    // });
+
+    fileTransfer.upload(body.cvUri, this.APILink + 'signup', options)
+      .then((data) => {
+      console.log(JSON.stringify(data)+" Uploaded Successfully");
+      loader.dismiss();
+      console.log("Image uploaded successfully");
+    }, (err) => {
+      console.log(err);
+      loader.dismiss();
+      alert(err);
     });
   }
+
 
 }
