9e174b36504e2c8edc9a5634f5c777d11e6c496c
Uxman <ZAKI_US94@HOTMAIL.COM>
2019-02-13 16:52:03 +0500

    home page compketed

--- src/pages/inspections-tab/inspections-tab.ts
+++ src/pages/inspections-tab/inspections-tab.ts
@@ -13,8 +13,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'inspections-tab.html',
 })
 export class InspectionsTabPage {
+  draftsArray:any;
+  urlIcon1 = "assets/imgs/inspection-drawer.png";
+  completedArray: { img: string; number: string; address: string; }[];
+  inProgressArray: { img: string; number: string; address: string; }[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.draftsArray = [
+
+      {img:this.urlIcon1 , number: 'IN34560701AR1', address: "draft address 1 Prince Faisal bin Fahd Rd., Khobar, Eastern..." },
+      {img:this.urlIcon1 , number: 'IN34560701AR2', address: "draft address 2 Faisal bin Fahd Rd., Khobar, Eastern..." },
+      {img:this.urlIcon1 , number: 'IN34560701AR3', address: "draft address 3 Faisal bin Fahd Rd., Khobar, Eastern..." },
+  
+    ]
+
+    this.inProgressArray = [
+
+      {img:this.urlIcon1 , number: 'IN34560701', address: "Progress address 1 Faisal bin Fahd Rd., Khobar, Eastern..." },
+      {img:this.urlIcon1 , number: 'IN34560701', address: "Progress address 2 bin Fahd Rd., Khobar, Eastern..." },
+      {img:this.urlIcon1 , number: 'IN34560701', address: "Progress address 3 bin Fahd Rd., Khobar, Eastern..." },
+  
+    ]
+
+
+    this.completedArray = [
+
+      {img:this.urlIcon1 , number: 'IN34560701', address: "completed address 1 bin Fahd Rd., Khobar, Eastern..." },
+      {img:this.urlIcon1 , number: 'IN34560701', address: "completed address 2 bin Fahd Rd., Khobar, Eastern..." },
+      {img:this.urlIcon1 , number: 'IN34560701', address: "completed address 3 bin Fahd Rd., Khobar, Eastern..." },
+  
+    ]
   }
 
   ionViewDidLoad() {
